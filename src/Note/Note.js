92e8298eac3cdde4274da7d34e515f52d5db619a
Madison Brown <59658823+mbcodes29@users.noreply.github.com>
2020-08-27 11:38:10 -0400

    fixed removal functionality

--- src/Note/Note.js
+++ src/Note/Note.js
@@ -23,10 +23,6 @@ export default class Note extends React.Component {
         "content-type": "application/json",
       },
     })
-      .then((res) => {
-        if (!res.ok) return res.json().then((e) => Promise.reject(e));
-        return res.json();
-      })
       .then(() => {
         this.context.deleteNote(noteId);
         // allow parent to perform extra behaviour
